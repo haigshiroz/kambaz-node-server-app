e32819650271d34c4da5baf52f4487152d0d40f7
haigshiroz <114354276+haigshiroz@users.noreply.github.com>
2025-04-01 15:31:00 -0400

    5.3.1

---
+++ Kambaz/Database/assignments.js
@@ -0,0 +1,122 @@
+[
+    {
+        "_id": "A101",
+        "title": "Propulsion Assignment",
+        "course": "RS101",
+        "date_available": "2024-05-06T00:01",
+        "date_due": "2024-05-07T00:01",
+        "date_until": "2024-05-08T00:01",
+        "points": 90,
+        "description": "This is description A101",
+        "assignment_group": "QUIZZES",
+        "display_grade_as": "POINTS",
+        "submission_type": "ONLINE"
+    },
+    {
+        "_id": "A102",
+        "title": "Combustion Analysis",
+        "course": "RS101",
+        "date_available": "2024-05-09T00:01",
+        "date_due": "2024-05-10T00:01",
+        "date_until": "2024-05-11T00:01",
+        "points": 50,
+        "description": "This is description A102",
+        "assignment_group": "EXAM",
+        "display_grade_as": "PERCENTAGE",
+        "submission_type": "ONLINE"
+    },
+    {
+        "_id": "A103",
+        "title": "Nozzle Design Project",
+        "course": "RS101",
+        "date_available": "2024-05-11T00:01",
+        "date_due": "2024-05-12T00:01",
+        "date_until": "2024-05-13T00:01",
+        "points": 100,
+        "description": "This is description A103",
+        "assignment_group": "ASSIGNMENTS",
+        "display_grade_as": "POINTS",
+        "submission_type": "ONLINE"
+    },
+    {
+        "_id": "A201",
+        "title": "Aerodynamics Quiz",
+        "course": "RS102",
+        "date_available": "2024-05-06T00:01",
+        "date_due": "2024-05-07T00:01",
+        "date_until": "2024-05-08T00:01",
+        "points": 1,
+        "description": "This is description A201",
+        "assignment_group": "EXAM",
+        "display_grade_as": "PERCENTAGE",
+        "submission_type": "ONLINE"
+    },
+    {
+        "_id": "A202",
+        "title": "Flow Analysis",
+        "course": "RS102",
+        "date_available": "2024-05-06T00:01",
+        "date_due": "2024-05-07T00:01",
+        "date_until": "2024-05-08T00:01",
+        "points": 10,
+        "description": "This is description A202",
+        "assignment_group": "EXAM",
+        "display_grade_as": "PERCENTAGE",
+        "submission_type": "ONLINE"
+    },
+    {
+        "_id": "A203",
+        "title": "Heating Analysis",
+        "course": "RS102",
+        "date_available": "2024-05-06T00:01",
+        "date_due": "2024-05-07T00:01",
+        "date_until": "2024-05-08T00:01",
+        "points": 200,
+        "description": "This is description A203",
+        "assignment_group": "QUIZZES",
+        "display_grade_as": "POINTS",
+        "submission_type": "ONLINE"
+    },
+
+
+
+    {
+        "_id": "A301",
+        "title": "Structural Design Task",
+        "course": "RS103",
+        "date_available": "2024-05-06T00:01",
+        "date_due": "2024-05-07T00:01",
+        "date_until": "2024-05-08T00:01",
+        "points": 100,
+        "description": "This is description A301",
+        "assignment_group": "QUIZZES",
+        "display_grade_as": "POINTS",
+        "submission_type": "ONLINE"
+    },
+    {
+        "_id": "A302",
+        "title": "Orbital Calculations",
+        "course": "RS103",
+        "date_available": "2024-05-06T00:01",
+        "date_due": "2024-05-07T00:01",
+        "date_until": "2024-05-08T00:01",
+        "points": 25,
+        "description": "This is description A302",
+        "assignment_group": "ASSIGNMENTS",
+        "display_grade_as": "PERCENTAGE",
+        "submission_type": "ONLINE"
+    },
+    {
+        "_id": "A303",
+        "title": "Systems Engineering Exam",
+        "course": "RS103",
+        "date_available": "2024-05-06T00:01",
+        "date_due": "2024-05-07T00:01",
+        "date_until": "2024-05-08T00:01",
+        "points": 40,
+        "description": "This is description A303",
+        "assignment_group": "QUIZZES",
+        "display_grade_as": "POINTS",
+        "submission_type": "ONLINE"
+    }
+]
