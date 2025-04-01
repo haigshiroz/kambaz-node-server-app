cb8ec898186a2b61e83eda3e9d8f76b40320c9fc
haigshiroz <114354276+haigshiroz@users.noreply.github.com>
2025-04-01 00:04:11 -0400

    5.2.2

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import Hello from "./Hello.js"
 import Lab5 from "./Lab5/index.js";
+import PathParameters from './Lab5/PathParameters.js';
 
 const app = express()
 
 Lab5(app);
-Hello(app)
+Hello(app);
+PathParameters(app);
 
 app.listen(process.env.PORT || 4000)
