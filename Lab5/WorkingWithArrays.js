9f4df3c91f7aedf1f16a00272c3fb47f71308eaa
haigshiroz <114354276+haigshiroz@users.noreply.github.com>
2025-04-01 15:02:45 -0400

    5.2 Done!!

--- Lab5/WorkingWithArrays.js
+++ Lab5/WorkingWithArrays.js
@@ -37,6 +37,14 @@ export default function WorkingWithArrays(app) {
         res.json(todos);
     });
 
+    // New create: uses post
+    app.post("/lab5/todos", (req, res) => {
+        const newTodo = { ...req.body, id: new Date().getTime() };
+        todos.push(newTodo);
+        res.json(newTodo);
+    });
+
+
     app.get("/lab5/todos/:id", (req, res) => {
         const { id } = req.params;
         const todo = todos.find((t) => t.id === parseInt(id));
@@ -54,6 +62,17 @@ export default function WorkingWithArrays(app) {
         todos.splice(todoIndex, 1);
         res.json(todos);
     });
+    app.delete("/lab5/todos/:id", (req, res) => {
+        const { id } = req.params;
+        const todoIndex = todos.findIndex((t) => t.id === parseInt(id));
+        if (todoIndex === -1) {
+            res.status(404).json({ message: `Unable to delete Todo with ID ${id}` });
+            return;
+        }
+        todos.splice(todoIndex, 1);
+        res.sendStatus(200);
+    });
+
 
     app.get("/lab5/todos/:id/title/:newTitle", (req, res) => {
         const { id, newTitle } = req.params;
@@ -90,5 +109,25 @@ export default function WorkingWithArrays(app) {
         todo.completed = newCompleted === "true";
         res.json(todos);
     });
+
+    app.put("/lab5/todos/:id", (req, res) => {
+        const { id } = req.params;
+        
+        // Check index exists
+        const todoIndex = todos.findIndex((t) => t.id === parseInt(id));
+        if (todoIndex === -1) {
+          res.status(404).json({ message: `Unable to update Todo with ID ${id}` });
+          return;
+        }
+
+        todos = todos.map((t) => {
+            if (t.id === parseInt(id)) {
+                return { ...t, ...req.body };
+            }
+            return t;
+        });
+        res.sendStatus(200);
+    });
+
 
 };
