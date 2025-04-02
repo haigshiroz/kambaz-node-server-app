191352a1db2c9244f2fa115402f10b3e817d0b1e
haigshiroz <114354276+haigshiroz@users.noreply.github.com>
2025-04-02 16:50:56 -0400

    5.3.7

--- Kambaz/Enrollments/dao.js
+++ Kambaz/Enrollments/dao.js
@@ -2,7 +2,21 @@ import Database from "../Database/index.js";
 import { v4 as uuidv4 } from "uuid";
 
 
+// Read
+export function enrollments() {
+    const { enrollments } = Database;
+    return enrollments;
+}
+
+// Create
 export function enrollUserInCourse(userId, courseId) {
     const { enrollments } = Database;
     enrollments.push({ _id: uuidv4(), user: userId, course: courseId });
+}
+
+// Delete
+export function unenrollUserInCourse(userId, courseId) {
+    const { enrollments } = Database;
+    Database.enrollments = enrollments.filter((e) => e.user !== userId || e.course !== courseId);
+    return Database.enrollments;
 }
--- Kambaz/Enrollments/routes.js
+++ Kambaz/Enrollments/routes.js
@@ -1,1 +1,26 @@
-// TODO ?
+import * as dao from "./dao.js";
+
+
+export default function EnrollmentRoutes(app) {
+    const enrollments = (req, res) => {
+        const enrollments = dao.enrollments();
+        res.send(enrollments);
+    };
+    app.get("/api/enrollments/", enrollments);
+
+
+    const createEnrollment = (req, res) => {
+        const { userId, courseId } = req.params;
+        const newEnrollment = dao.enrollUserInCourse(userId, courseId);
+        res.send(newEnrollment);
+    };
+    app.post("/api/enrollments/:userId/:courseId", createEnrollment);
+
+
+    const deleteEnrollment = (req, res) => {
+        const { userId, courseId } = req.params;
+        const status = dao.unenrollUserInCourse(userId, courseId);
+        res.send(status);
+    };
+    app.delete("/api/enrollments/:userId/:courseId", deleteEnrollment)
+}
